7d187b1c25d6fd00518ae7fc2a13f0e728775890
Hailey <carly1982@naver.com>
2023-07-25 23:25:43 +0900

    First Commit
    Restart write turtle_crossing_game code.

---
+++ car_manager.py
@@ -0,0 +1,13 @@
+from turtle import Turtle 
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+X = -300
+STARTING_POSITIONS = [(X, -225), (X, -200), (X, -175), (X, -150), (X, -125), (X, -100), (X, -75), (X, -50), (X, -25),
+                      (X, 0), (X, 25), (X, 50), (X, 75), (X, 100), (X, 125), (X, 150), (X, 175), (X, 200), (X, 225)]
+
+class CarManager:
+    pass
+        
+    
---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+player = Player() 
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
