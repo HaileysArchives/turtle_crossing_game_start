b100d7a912e41acc06089a744eed84447dfa84b8
Hailey <carly1982@naver.com>
2023-07-25 23:46:59 +0900

    Second Commit
    Write about car_manager.py code. (def __init__(), def make_cars())

--- car_manager.py
+++ car_manager.py
@@ -8,6 +8,17 @@ STARTING_POSITIONS = [(X, -225), (X, -200), (X, -175), (X, -150), (X, -125), (X,
                       (X, 0), (X, 25), (X, 50), (X, 75), (X, 100), (X, 125), (X, 150), (X, 175), (X, 200), (X, 225)]
 
 class CarManager:
-    pass
+    
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        
+        for car in self.all_cars:
+            new_car = Turtle()
+            new_car.color(random.choice(COLORS))
+            new_car.shape("square")
+            new_car = car
+            self.all_cars.append(car)
         
     
